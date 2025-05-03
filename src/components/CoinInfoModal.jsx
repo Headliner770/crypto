18912248a6f07d4acdda4f9e095ec413d318d1a4
Headliner770 <paul248022@gmail.com>
2025-05-03 21:14:21 +0300

    go

--- src/components/CoinInfoModal.jsx
+++ src/components/CoinInfoModal.jsx
@@ -1,3 +1,16 @@
+import { Flex, Tag, Typography } from "antd";
+
 export default function CoinInfoModal({ coin }) {
-  return <h2>{coin.name}</h2>;
+  return (
+    <Flex align="center">
+      <img
+        src={coin.icon}
+        alt={coin.name}
+        style={{ width: 40, marginRight: 20 }}
+      />
+      <Typography.Title level={2} style={{ margin: 0 }}>
+        ({coin.symbol}) {coin.name}
+      </Typography.Title>
+    </Flex>
+  );
 }
